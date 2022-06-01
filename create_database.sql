abe2c3fa829d25b112a89625097e2db41cec69e0
jarwix <oazisrus@gmail.com>
2022-06-01 14:56:29 +0500

    update base

--- create_database.sql
+++ create_database.sql
@@ -2,8 +2,8 @@ CREATE SCHEMA `telegramtaskbot` DEFAULT CHARACTER SET utf8mb4 ;
 
 CREATE TABLE `telegramtaskbot`.`tasksdata` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `idtg` VARCHAR(100) NOT NULL,
-  `tasks` VARCHAR(45) NOT NULL,
+  `idtg` VARCHAR(20) NOT NULL,
+  `tasks` VARCHAR(100) NOT NULL,
   `time` DATETIME NULL DEFAULT NULL,
   PRIMARY KEY (`id`));
 );
